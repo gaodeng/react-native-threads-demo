3d3eb47b82f66bd94f44bb17895371592a47e37a
gaodeng <gaodengming@gmail.com>
2023-12-27 19:00:31 +0800

    add react-native-skia

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import { Canvas, Circle, Group } from "@shopify/react-native-skia";
+
 import { Thread } from 'react-native-threads';
+import { useState } from 'react';
 
 // start a new react native JS process
 const thread = new Thread('./thread.js');
 
 export default function App() {
+  const [msg, setMsg] = useState('');
+  const width = 256;
+  const height = 256;
+  const r = width * 0.33;
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>{msg}</Text>
       <Button title='hello' onPress={()=>{
-          // send a message, strings only
           thread.postMessage('hello from app.js');
-          // listen for messages
-          thread.onmessage = (message) => console.log(message);
+          thread.onmessage = (message) => {console.log(message)
+            setMsg(message);
+          };
       }}></Button>
       <StatusBar style="auto" />
+      <View style={{borderWidth: 2, borderColor: 'red'}}>
+      <Canvas style={{ width, height }}>
+      <Group blendMode="multiply">
+        <Circle cx={r} cy={r} r={r} color="cyan" />
+        <Circle cx={width - r} cy={r} r={r} color="magenta" />
+        <Circle cx={width / 2} cy={width - r} r={r} color="yellow" />
+      </Group>
+    </Canvas>
+    </View>
     </View>
   );
 }
--- thread.js
+++ thread.js
@@ -3,7 +3,7 @@ import { self } from 'react-native-threads';
 // listen for messages
 self.onmessage = (message) => {
 
-    self.postMessage('hello from thread.js');
+    self.postMessage('hello from thread.js ' + new Date().toISOString());
 }
 
 // send a message, strings only
